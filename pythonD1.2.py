4037ab7bf5f7c8104481e1a5a875c2979e2d5b5d
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2022-08-06 16:28:25 +0530

    Add files via upload
    DAY1 : content

---
+++ pythonD1.2.py
@@ -0,0 +1,6 @@
+# name = input("enter your name:")
+# count=0
+# for i in name:
+#     count+=1
+# print(count)
+print(len(input("enter your name:")))
---
+++ pythonD1project.py
@@ -0,0 +1,4 @@
+print("Welcome to the brand name generator")
+var1=input("What's the name of the city in which u were born?\n")
+var2=input("What's ur pet's name?\n")
+print("Ur band name would be:",var1 , var2)
